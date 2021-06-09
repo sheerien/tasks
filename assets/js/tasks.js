948aa56633e19f11e38af9fbef4e34634ebc9500
sheerien <freemebabaj@gmail.com>
2021-06-09 14:18:24 +0300

    get images and show it

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -4,6 +4,7 @@ var boxPopup = document.getElementById("box-popup");
 var popup = document.getElementById("popup");
 var req = new XMLHttpRequest();
 
+
 // create html template
 function templateData(data) {
     return template = `<div class="col">
@@ -29,10 +30,17 @@ function fetchAllData(req, tempHtml, url) {
                 temp += templateData(data[i])
             }
             tempHtml.innerHTML = temp;
-            var tagElements = document.getElementsByTagName("h1")
+            var tagElements = document.getElementsByTagName("h1");
             var tagArray = Array.from(tagElements);
             for (let i = 0, len = tagArray.length; i < len; i++) {
                 tagArray[i].addEventListener("click", getInfo);
+            }
+
+            var tagImages = document.getElementsByTagName("img");
+            imgArray = Array.from(tagImages);
+            for (let i = 0, len = imgArray.length; i < len; i++) {
+                imgArray[i].addEventListener("click", getImages)
+
             }
         }
     }
@@ -62,6 +70,70 @@ function fetchUserData(req, url, userName) {
     console.log(`${url}/${userName}`)
     req.open("GET", `${url}/${userName}`);
     req.send();
+}
+
+function getImages(e) {
+    console.log(e.target.src)
+    boxPopup.style.transform = "scale(1, 1)"
+    popup.style.backgroundImage = `url(${e.target.src})`
+    indexTargetImage = 0
+    indexTargetImage = imgArray.indexOf(e.target);
+    // console.log(indexTargetImage)
+
+    var closeWindow = document.getElementById("close");
+    var leftWindow = document.getElementById("left");
+    var rightWindow = document.getElementById("right");
+    closeWindow.addEventListener("click", closeWin)
+    leftWindow.addEventListener("click", goBack)
+
+    rightWindow.addEventListener("click", goNext)
+}
+
+function closeWin() {
+
+    boxPopup.style.transform = "scale(0, 0)"
+}
+
+function goBack(e) {
+    indexTargetImage--
+    if (indexTargetImage < 0) {
+        indexTargetImage = imgArray.length - 1
+    }
+    popup.style.backgroundImage = `url(${imgArray[indexTargetImage].src})`
+}
+
+
+function goNext(e) {
+    indexTargetImage++
+    if (indexTargetImage == imgArray.length) {
+        indexTargetImage = 0
+    }
+    popup.style.backgroundImage = `url(${imgArray[indexTargetImage].src})`
+}
+
+document.addEventListener("keydown", keysOptions)
+
+function keysOptions(e) {
+    // console.log(e.key);
+    switch (e.key) {
+        case "ArrowRight":
+            goNext(e)
+            break;
+        case "ArrowLeft":
+            goBack(e)
+            break;
+        case "Escape":
+            closeWin(e)
+            break;
+        case " ":
+            closeWin(e)
+            break;
+
+        default:
+            break;
+    }
+    // ArrowRight - ArrowLeft - Escape - " "
+
 }
 
 (function() {
